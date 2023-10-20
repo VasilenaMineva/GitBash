ace3a1f3182732cb2af43cc129d65d1c1d9ac619
littleH3r0 <74876631+vasilena6mariq@users.noreply.github.com>
2023-10-20 12:06:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_bash
+{
+    internal class Program
+    {
+        public const string FILE_NAME = "example.bin";
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(FILE_NAME, FileMode.CreateNew);
+            BinaryWriter bw = new BinaryWriter(fs);
+            for (int i = 0; i < 11; i++)
+            {
+                bw.Write(1);
+            }
+            bw.Close();
+            fs.Close();
+            using (fs = new FileStream(FILE_NAME, FileMode.Open, FileAccess.Read))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                for (int i = 0; i<11; i ++)
+                {
+                    Console.WriteLine(br.ReadInt32());
+                }
+
+            }
+            fs.Close();
+        }
+    }
+}
